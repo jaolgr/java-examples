fb43fb3ae94076e798c7009d1d5c7b06e4aea23a
jaolgr <jaolgr@gmail.com>
2019-05-10 16:17:30 +0200

    Imprimir mensaje

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hello world");
+
+        String name = "jaume";
+        String message = "hola " + name;
+
+        System.out.println(message);
     }
 }
