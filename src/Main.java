05fa35958f68a0c427b5134291428bc9d0b059e4
jaolgr <jaolgr@gmail.com>
2019-05-10 16:37:37 +0200

    Primer cálculo

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,19 @@ public class Main {
         String name = "jaume";
         String message = "hola " + name;
 
+        // shortcut: sout
         System.out.println(message);
+
+        //tipos de variables
+        // String - texto
+        // int - num enteros
+        // double - num con decimales
+        // boolean -  true/false
+
+        int quantity = 3;
+        double price = 1.25;
+        double total = quantity * price;
+
+        System.out.println("El total es: " + total);
     }
 }
